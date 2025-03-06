a252c874b63c2ccb9807762904632127ebfbbdf7
Eduardo Carvalho <ducas1912@gmail.com>
2025-03-06 17:26:13 +0000

    dd

--- script.js
+++ script.js
@@ -201,3 +201,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
